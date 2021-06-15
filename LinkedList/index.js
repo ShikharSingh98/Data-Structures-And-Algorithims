6370f43d9b560d66b7915d197ca8bb5c0d90f56a
ShikharSingh98 <shikharsingh98@gmail.com>
2021-06-16 01:17:28 +0530

    Insert a node in Linked List

---
+++ LinkedList/index.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(data) {
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+
+      while (current.next !== null) {
+        current = current.next;
+      }
+
+      current.next = newNode;
+    }
+  }
+
+  display() {
+    let current = this.head;
+
+    while (current !== null) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+let ll = new LinkedList();
+
+ll.insert(10);
+ll.insert(20);
+ll.insert(30);
+ll.insert(40);
+ll.insert(50);
+
+ll.display();
